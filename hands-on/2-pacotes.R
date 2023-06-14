9403c804d280b38b3461bc5afcc1f963c981fbe6
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2023-06-14 19:26:31 -0300

    hands-on

---
+++ hands-on/2-pacotes.R
@@ -0,0 +1,10 @@
+#Instalando um novo pacote
+
+#Instalando o pacote ggplot2
+install.packages("ggplot2")
+
+#Carregando o pacote ggplot2
+library(ggplot2)
+
+# ggplot 2 é um pacote de visualização de dados - utilizado para a criação de gráficos
+?ggplot2
