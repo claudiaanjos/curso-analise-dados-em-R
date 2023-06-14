26b44de997e113dda0c61fbc882fb6c62cfa51c6
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2023-06-14 19:25:40 -0300

    hands-on

---
+++ hands-on/1-script.R
@@ -0,0 +1,33 @@
+#Criando o seu primeiro script
+
+#Atribuindo texto à variável mensagem
+mensagem <- "Hello world!"
+
+#Imprimindo o valor da variável no console
+print(mensagem)
+
+#Isto é um comentário
+
+#Os comentários podem ser utilizados para ajudar outros programadores a entender o seu código
+
+
+#Para comentar basta inserir o caractere # no ínicio da linha 
+#Com isso o interpretador da linguagem R irá ignorar o que for digitado
+#Caso contrário será exibida uma mensagem de erro
+
+#Acessando a documentação de uma função
+#basta adicionar o caractere de interrogação (?) na frente do nome da função
+
+#Exemplo: Consultando a documentação da função print
+?print
+
+#O resultado irá aparecer na aba Help
+
+#Outras funções úteis
+?head
+?str
+?getwd
+?mean
+?sd
+?sum
+?seq
